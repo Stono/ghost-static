54f01fc6fc87d2b532e367d65e902b363e8eee1f
Karl Stoney <karl.stoney@autotrader.co.uk>
2019-01-02 19:02:10 +0000

    date in path

--- bin/generate_static_content.sh
+++ bin/generate_static_content.sh
@@ -4,5 +4,7 @@ rm -rf static
 mkdir -p static/content
 cp -R $GHOST_HOME/current/content/images static/content/
 
+echo "Running gssg..."
 gssg --url 'http://127.0.0.1:2368' --dest static
+echo "Static content generated!  Uploading to gcs..."
 gsutil -m rsync -R static gs://karlstoney.com
